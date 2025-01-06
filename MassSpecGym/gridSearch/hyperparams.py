ea1db0ff22bcca10dacde3585bd87d2e9b97ff11
Wout Welvaert <wout.welvaert@ugent.be>
2025-01-06 14:26:24 +0100

    gridsearch

--- MassSpecGym/gridSearch/hyperparams.py
+++ MassSpecGym/gridSearch/hyperparams.py
@@ -1,23 +1,33 @@
 import numpy as np
 from decimal import Decimal
 import itertools
+from collections import OrderedDict
 
-lr = [10 ** -x for x in np.arange(3, 4.01, 0.5)]
-batch_size = [512, 1024, 2048]
-d_model = [128, 256, 512]
-nhead = [2, 4, 8]
-num_encoder_layers = [2, 3, 4]
-num_decoder_layers = [2, 3, 4]
-#temperature = [0.8, 1.0, 1.2]
+hyperparams = OrderedDict({
+    "lr": [10 ** -x for x in np.arange(3, 4.01, 0.5)],
+    "batch_size": [512, 1024, 2048],
+    "d_model": [128, 256, 512],
+    "nhead": [2, 4, 8],
+    "num_encoder_layers": [2, 3, 4],
+    "num_decoder_layers": [2, 3, 4],
+    "temperature": [1.0],
+    "sampler": ["top-k"],
+    "k": [10],
+})
 
-print(f"DEBUG:lr {['%.2E' % Decimal(x) for x in lr]}")
-print(f"DEBUG:batch size {batch_size}")
-print(f"DEBUG:d_model {d_model}")
-print(f"DEBUG:nhead {nhead}")
-print(f"DEBUG:num_encoder_layers {num_encoder_layers}")
-print(f"DEBUG:num_decoder_layers {num_decoder_layers}")
-#print(f"DEBUG:temperature {temperature}")
-assert False
-for _lr, _batch_size, _d_model, _nhead, _num_enc, _num_dec in itertools.product(lr, batch_size, d_model, nhead, num_encoder_layers, num_decoder_layers):
-    _temp = 1.0
-    print(f"--lr {_lr} --batch_size {_batch_size} --d_model {_d_model} --nhead {_nhead} --num_encoder_layers {_num_enc} --num_decoder_layers {_num_dec} --temperature {_temp}")
+for k, v in hyperparams.items():
+    print("DEBUG:", k, ":", (v if k != "lr" else ['%.2E' % Decimal(x) for x in v]))
+
+shortened_params = {
+    "lr": "lr",
+    "batch_size": "bs",
+    "d_model": "dmod",
+    "nhead": "nh",
+    "num_encoder_layers": "enc",
+    "num_decoder_layers": "dec",
+}
+
+for values in itertools.product(*hyperparams.values()):
+    args = " ".join(["--" + k + " " + str(v) for k, v in zip(hyperparams.keys(), values)])
+    jobname = "_".join([shortened_params[k] + ((str(round(v)) if type(v) != str else v) if k != "lr" else '%.0E' % Decimal(v)) for k, v in zip(hyperparams.keys(), values) if k in shortened_params])
+    print(jobname + "|" + args)
---
+++ MassSpecGym/gridSearch/jobqueuer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PROJECT="GridSearch"
+
+python3 hyperparams.py | while IFS= read -r line; do
+    JOBNAME=$(echo $line | cut -d "|" -f1)
+    ARGS=$(echo $line | cut -d "|" -f2-)
+
+    if [[ $JOBNAME == DEBUG* ]]; then
+        # Handle debug lines
+        echo $ARGS
+    else
+        echo $JOBNAME
+        # Create a unique job script
+        job_script=$(mktemp)
+        cat <<EOF > $job_script
+#!/bin/bash
+#PBS -N $JOBNAME
+#PBS -l walltime=10:00:00
+#PBS -l gpus=1
+#PBS -l mem=32gb
+
+cd data/Thesis
+
+source python3-11-venv/bin/activate
+
+export WANDB_API_KEY="c98a88c3a37fb338089fe9d5d9b71abfc376a8d3"
+
+python run.py --job_key 1 --run_name $JOBNAME --max_epochs 100 --task de_novo --model smiles_transformer --log_only_loss_at_stages "train, val" --project_name $PROJECT --check_val_every_n_epoch 1 --patience 5 $ARGS
+EOF
+
+        # Submit the job
+        qsub $job_script
+
+        # Clean up the temporary job script
+        rm -f $job_script
+    fi
+done
