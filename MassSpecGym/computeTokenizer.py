54811d81220ad6e769d3e1a24d3bb585811780e6
Wout Welvaert <wout.welvaert@ugent.be>
2025-03-04 11:40:29 +0100

    tokenizers vocab_size

--- MassSpecGym/computeTokenizer.py
+++ MassSpecGym/computeTokenizer.py
@@ -7,14 +7,15 @@ import sys
 cache_dir = "cache"
 
 def main():
-    # Ensure exactly 2 arguments are provided (excluding the script name)
-    if len(sys.argv) != 3:
+    # Ensure exactly 3 arguments are provided (excluding the script name)
+    if len(sys.argv) != 4:
         print("Error: Two arguments are required: representation and dataset_size.")
         sys.exit(1)
     
     # Get the arguments and process them
     representation = sys.argv[1].lower()
     dataset_size_str = sys.argv[2]
+    vocab_size_str = sys.argv[3]
     
     # Validate the representation
     if representation not in ["smiles", "selfies"]:
@@ -24,15 +25,19 @@ def main():
     # Validate and convert dataset_size to an integer
     try:
         dataset_size = int(dataset_size_str)
+        vocab_size = int(vocab_size_str)
     except ValueError:
-        print("Error: dataset_size must be an integer.")
+        print("Error: dataset_size and vocab_size must be integers.")
         sys.exit(1)
-    
+
+    assert False
+
+
     if representation == "smiles":
-        tokenizer = SmilesBPETokenizer(dataset_size=dataset_size, max_len=200, cache_dir=cache_dir)
+        tokenizer = SmilesBPETokenizer(dataset_size=dataset_size, max_len=200, cache_dir=cache_dir, vocab_size=vocab_size)
     
     # store tokenizer
-    filename = f"tokenizers/{representation}_tokenizer_{dataset_size}M.pkl"
+    filename = f"tokenizers/{representation}_tokenizer_{dataset_size}M_vocab_{vocab_size}.pkl"
     with open(filename, 'wb') as f:
         pickle.dump(tokenizer, f)
 
--- MassSpecGym/mol_tokenizers.py
+++ MassSpecGym/mol_tokenizers.py
@@ -135,7 +135,7 @@ class SelfiesTokenizer(SpecialTokensBaseTokenizer):
 
 
 class SmilesBPETokenizer(SpecialTokensBaseTokenizer):
-    def __init__(self, dataset_size: int=4, smiles_pth: T.Optional[str] = None, cache_dir=None, **kwargs):
+    def __init__(self, dataset_size: int=4, smiles_pth: T.Optional[str] = None, cache_dir=None, vocab_size=30000, **kwargs):
         """
         Initialize the BPE tokenizer for SMILES strings, with optional training data.
 
@@ -150,6 +150,10 @@ class SmilesBPETokenizer(SpecialTokensBaseTokenizer):
             smiles = utils.load_unlabeled_mols(col_name="smiles", size=dataset_size, cache_dir=cache_dir).tolist()
             smiles += utils.load_train_mols().tolist()
             print(f"Training tokenizer on {len(smiles)} SMILES strings.")
-            tokenizer.train_from_iterator(smiles)
+            tokenizer.train_from_iterator(smiles, vocab_size)
 
         super().__init__(tokenizer, **kwargs)
+
+# TODO
+#class SelfiesBPETokenizer(SpecialTokensBaseTokenizer):
+    
